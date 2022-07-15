0af1e7308e5b4b9c59314a7a607f268936e2ea14
THANHTRUYEN <truyenltt.it@gmail.com>
2022-07-15 17:10:27 +0700

    cart

--- src/Component/delivery/DeliveryPayment.js
+++ src/Component/delivery/DeliveryPayment.js
@@ -4,11 +4,17 @@ import Payment from "./payment/Payment";
 export default function DeliveryPayment() {
   return (
     <div className=" bg-light h-100">
-      <div className="d-flex justify-content-md-center ">
-        <div className="d-inline m-2 bg-white text-dark col-8 col-lg-4">
+      <div
+        className="d-flex"
+        style={{
+          paddingLeft: "250px",
+          paddingRight: "250px",
+        }}
+      >
+        <div className="d-inline m-2 bg-white text-dark col-8 ">
           <DeliveryInformation />
         </div>
-        <div className="d-inline m-2 bg-white text-dark col-4 col-lg-4">
+        <div className="d-inline m-2 bg-white text-dark col-4">
           <Payment />
         </div>
       </div>
